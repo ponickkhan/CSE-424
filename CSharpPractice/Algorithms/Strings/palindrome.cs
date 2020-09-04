4098cbe0744d00dbee020bd7eac1d157e46bbcfd
Md.Rafiuzzaman Khan <ponickid@gmail.com>
2020-09-04 23:46:59 +0600

    Algorithm test in C#

---
+++ CSharpPractice/Algorithms/Strings/palindrome.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace CSharpPractice.Algorithms.Strings
+{
+    
+    public class Palindrome
+    {
+        
+        public static bool IsStringPalindrome(string word) =>
+            TypifyString(word).Equals(TypifyString(ReverseString(word)));
+
+        private static string TypifyString(string word)
+        {
+            // Typify string to lower and remove white spaces.
+            return Regex.Replace(word.ToLowerInvariant(), @"\s+", string.Empty);
+        }
+
+        private static string ReverseString(string s)
+        {
+            var arr = s.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+    }
+}
---
+++ CSharpPractice/Algorithms/Tests.cs
@@ -0,0 +1,15 @@
+using CSharpPractice.Algorithms.Strings;
+
+
+namespace Algorithms.Tests.Strings
+{
+    class Tests
+    {
+        
+     Palindrome Myob = new Palindrome();
+
+     Myob.IsStringPalindrome("Hello");
+
+    }
+   
+    }
