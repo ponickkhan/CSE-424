d854a7f146ffb46f47a4d92d15dcedcd41d27d10
Md.Rafiuzzaman Khan <ponickid@gmail.com>
2020-09-27 05:34:39 +0600

    mid term practice problem 3 armstrong number

--- CSharpPractice/AssignmentOne.cs
+++ CSharpPractice/AssignmentOne.cs
@@ -63,13 +63,32 @@ namespace CSharpPractice
          
         }
 
+        static void armstrong_number()
+        {
+            int num,r,sum=0,temp;
+	
+	
+            Console.Write("\n\n");
+            Console.Write("Check whether a given number is armstrong number or not:\n");
+            Console.Write("----------------------------------------------------------");
+            Console.Write("\n\n");  	
+
+            Console.Write("Input  a number: ");
+            num = Convert.ToInt32(Console.ReadLine());	
+            for(temp=num;num!=0;num=num/10){
+                r=num % 10;
+                sum=sum+(r*r*r);
+            }
+
+            Console.Write(sum == temp ? "{0} is an Armstrong number.\n" : "{0} is not an Armstrong number.\n", temp);
+        }
+
         public static void Main()
         {
             //Perfect_Number();
+            armstrong_number();
             int a = 98, b = 56;
-            Console.WriteLine("GCD of "
-                              + a +" and " + b + " is "
-                              + gcd(a, b));
+           // Console.WriteLine("GCD of " + a +" and " + b + " is " + gcd(a, b));
         }
 
     }
