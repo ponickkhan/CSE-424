466c0156bec8ec4f8c0ec98f0dcc1714839c7b63
Md.Rafiuzzaman Khan <ponickid@gmail.com>
2020-10-17 15:08:14 +0600

    mid exam problem solution 3

---
+++ CSharpPractice/MidProb3.cs
@@ -0,0 +1,50 @@
+using System;
+
+namespace CSharpPractice
+{
+    public class MidProb3
+    {
+        public static void Perfect_Number()
+        {
+            Console.Write("\n\n");
+            Console.WriteLine("Check whether a given number is perfect number or not:");
+            Console.WriteLine("------------------------------------------------------");
+
+            Console.Write("Enter The Number : ");
+            Int64 n = Convert.ToInt64(Console.ReadLine());
+
+            var sum = 1;
+
+            for (var i = 2; i * i <= n; i++)
+            {
+                if (i > 0)
+                    if (n % i != 0)
+                        continue;
+                if (i * i != n)
+                {
+                    sum = (int) (sum + i + n / i);
+                }
+                else
+                    sum = sum + i;
+            }
+
+            if (sum == n && n != 1)
+            {
+                Console.WriteLine($"{n} is a perfect number ! ");
+            }
+            else
+            {
+                Console.WriteLine($"{n} is not a perfect number ! ");
+            }
+
+
+
+        }
+
+        public static void Main()
+        {
+            Perfect_Number();
+
+        }
+    }
+}
